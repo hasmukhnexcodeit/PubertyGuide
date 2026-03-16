a2c952436060ee3e6d95322c0f735e66f432f2b2
hasmukhnexcodeit <hasmukh@nexcodeit.com>
2026-03-16 15:29:43 +0530

    child new file add

--- wp-content/themes/astra-child/functions.php
+++ wp-content/themes/astra-child/functions.php
@@ -3,32 +3,22 @@
  * Astra Child Theme Functions
  */
 
-function astra_child_enqueue_assets() {
 
-    // Child theme stylesheet
-    wp_enqueue_style(
-        'astra-child-style',
-        get_stylesheet_uri(),
-        array('astra-theme-css'),
-        '1.0'
-    );
-
-    // Custom CSS
-    wp_enqueue_style(
-        'astra-child-custom-css',
-        get_stylesheet_directory_uri() . '/assets/css/custom.css',
-        array(),
-        '1.0'
-    );
-
-    // Custom JS
-    wp_enqueue_script(
-        'astra-child-custom-js',
-        get_stylesheet_directory_uri() . '/assets/js/custom.js',
-        array('jquery'),
-        '1.0',
-        true
-    );
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
 }
 
-add_action('wp_enqueue_scripts', 'astra_child_enqueue_assets');
+/**
+ * Constants
+ */
+define( 'ASTRA_CHILD_VERSION', '1.0.0' );
+define( 'ASTRA_CHILD_DIR', trailingslashit( get_stylesheet_directory() ) );
+define( 'ASTRA_CHILD_URI', trailingslashit( get_stylesheet_directory_uri() ) );
+define( 'ASTRA_CHILD_INC', ASTRA_CHILD_DIR . 'inc/' );
+
+/**
+ * Include Files
+ */
+require_once ASTRA_CHILD_INC . 'enqueue-assets.php';
+require_once ASTRA_CHILD_INC . 'inc/custom-function.php';
+ 
---
+++ wp-content/themes/astra-child/inc/enqueue-assets.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Enqueue Styles & Scripts
+ *
+ * @package Astra_Child
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+}
+
+function astra_child_enqueue_assets() {
+
+    // Child theme stylesheet
+    wp_enqueue_style(
+        'astra-child-style',
+        get_stylesheet_uri(),
+        array('astra-theme-css'),
+        '1.0'
+    );
+
+    // Custom CSS
+    wp_enqueue_style(
+        'astra-child-custom-css',
+        get_stylesheet_directory_uri() . '/assets/css/custom.css',
+        array(),
+        '1.0'
+    );
+
+    // Custom JS
+    wp_enqueue_script(
+        'astra-child-custom-js',
+        get_stylesheet_directory_uri() . '/assets/js/custom.js',
+        array('jquery'),
+        '1.0',
+        true
+    );
+}
+
+add_action('wp_enqueue_scripts', 'astra_child_enqueue_assets');
